a28fb8c1263798e4f37920a4a7aa98076a0dfadf
stasrndn <stas.rndn@yandex.ru>
2022-09-06 17:51:21 +0300

    Убирает лишние свойства в модели

--- src/model/films-model.js
+++ src/model/films-model.js
@@ -6,13 +6,9 @@ import {MAX_FILMS_COUNT} from '../const.js';
  */
 export default class FilmsModel {
   #films = [];
-  #comments = [];
-  #commentsModel = null;
 
   constructor(commentsModel) {
-    this.#commentsModel = commentsModel;
-    this.#comments = [...this.#commentsModel.getComments()];
-    this.#films = Array.from({length: MAX_FILMS_COUNT}, generateFilm.bind(this, this.#comments));
+    this.#films = Array.from({length: MAX_FILMS_COUNT}, generateFilm.bind(this, commentsModel.getComments()));
   }
 
   getFilms = () => this.#films;
