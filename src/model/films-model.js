0e75f54f9daa57337244777f747485f3dc705400
stasrndn <stas.rndn@yandex.ru>
2022-09-26 17:23:57 +0300

    Модуль 7.2
    Добавляет ещё один метод для обновления конкретного фильма.

--- src/model/films-model.js
+++ src/model/films-model.js
@@ -34,4 +34,25 @@ export default class FilmsModel extends Observable {
 
     this._notify(updateType, update);
   };
+
+  /**
+   * Метод для обновления фильма
+   * @param updateType
+   * @param update
+   */
+  updateFilm = (updateType, update) => {
+    const index = this.#films.findIndex((film) => film.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t update unexisting film');
+    }
+
+    this.#films = [
+      ...this.#films.slice(0, index),
+      update,
+      ...this.#films.slice(index + 1),
+    ];
+
+    this._notify(updateType, update);
+  };
 }
