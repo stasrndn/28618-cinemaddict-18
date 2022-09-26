decb5a8beb11d0f020d242bb8279208396d0e6eb
stasrndn <stas.rndn@yandex.ru>
2022-09-26 17:11:27 +0300

    Модуль 7.1
    Добавляет в модель для фильмов метод для их записи.

--- src/model/films-model.js
+++ src/model/films-model.js
@@ -1,17 +1,37 @@
+import Observable from '../framework/observable';
 import {generateFilm} from '../mock/film.js';
 import {MAX_FILMS_COUNT} from '../const.js';
 
 /**
  * Модель рыба для списка фильмов
  */
-export default class FilmsModel {
+export default class FilmsModel extends Observable {
   #films = [];
 
   constructor(commentsModel) {
+    super();
     this.#films = Array.from({length: MAX_FILMS_COUNT}, generateFilm.bind(this, commentsModel.comments));
   }
 
+  /**
+   * Метод для получения фильмов
+   * @returns {*[]}
+   */
   get films() {
     return this.#films;
   }
+
+  /**
+   * Метод для добавления фильма
+   * @param updateType
+   * @param update
+   */
+  addFilm = (updateType, update) => {
+    this.#films = [
+      update,
+      ...this.#films,
+    ];
+
+    this._notify(updateType, update);
+  };
 }
