4a7e6974613cdffff0c1b4a6a817ca19b82b4921
stasrndn <stas.rndn@yandex.ru>
2022-09-26 17:19:09 +0300

    Модуль 7.1.1
    Добавляет в модель для комментариев метод для их записи.

--- src/model/comments-model.js
+++ src/model/comments-model.js
@@ -1,13 +1,32 @@
+import Observable from '../framework/observable';
 import {generateComment} from '../mock/comment.js';
 import {MAX_COMMENTS_COUNT} from '../const.js';
 
 /**
  * Модель рыба для комментариев к фильмам
  */
-export default class CommentsModel {
+export default class CommentsModel extends Observable {
   #comments = Array.from({length: MAX_COMMENTS_COUNT}, generateComment);
 
+  /**
+   * Метод для получения комментариев
+   * @returns {unknown[]}
+   */
   get comments() {
     return this.#comments;
   }
+
+  /**
+   * Метод для добавления комментария
+   * @param updateType
+   * @param update
+   */
+  addComment = (updateType, update) => {
+    this.#comments = [
+      update,
+      ...this.#comments,
+    ];
+
+    this._notify(updateType, update);
+  };
 }
