e8ad2939bc9f421629bc31c8ca84322588a236ba
stasrndn <stas.rndn@yandex.ru>
2022-09-30 21:18:14 +0300

    Модуль 7
    Добавляет биб-ку he и выводит текст комментариев через энкодинг

--- src/view/comments-view.js
+++ src/view/comments-view.js
@@ -1,7 +1,8 @@
 import AbstractStatefulView from '../framework/view/abstract-stateful-view.js';
+import {isCtrlEnterPressed} from '../utils.js';
 import {EMOTIONS_LIST, UpdateType, UserAction} from '../const.js';
 import dayjs from 'dayjs';
-import {isCtrlEnterPressed} from '../utils';
+import he from 'he';
 
 /**
  * Шаблон элемента в блоке эмоций в форме добавления комментария
@@ -71,7 +72,7 @@ const createCommentsListItemTemplate = (comment) => (
       <img src="./images/emoji/${comment.emotion}.png" width="55" height="55" alt="emoji-${comment.emotion}">
     </span>
     <div>
-      <p class="film-details__comment-text">${comment.comment}</p>
+      <p class="film-details__comment-text">${he.encode(comment.comment)}</p>
       <p class="film-details__comment-info">
         <span class="film-details__comment-author">${comment.author}</span>
         <span class="film-details__comment-day">${dayjs(comment.date).format('YYYY/MM/DD HH:mm')}</span>
